608dc9b2af32c8aa44730515e81e9de7fa04abb1
zayaida-ux <zayaida.uxcare@gmail.com>
2026-05-01 11:09:53 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
